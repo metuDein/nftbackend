d4497f46f61d14fbc392eae4f3c8394f850148a4
metudein <maxmetadein@gmail.com>
2023-06-19 07:48:15 +0100

    new update

--- config/whiteList.js
+++ config/whiteList.js
@@ -3,7 +3,8 @@ const whiteList = [
     'http://localhost:3000',
     'http://tritonsprime.io',
     'https://tritonsprime.io',
-    'https://648ff29e11e1f62acd64fc3b--capable-gumdrop-792b50.netlify.app/'
+    'https://648ff29e11e1f62acd64fc3b--capable-gumdrop-792b50.netlify.app/',
+    'http://648ff29e11e1f62acd64fc3b--capable-gumdrop-792b50.netlify.app/'
 ]
 
 module.exports = whiteList
