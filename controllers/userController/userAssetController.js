ba63a2de8b5d20c40eeca3ae77fa035717ac7421
metudein <maxmetadein@gmail.com>
2023-06-22 12:09:33 +0100

    update

--- controllers/userController/userAssetController.js
+++ controllers/userController/userAssetController.js
@@ -58,15 +58,31 @@ const editAsset = async (req, res) => {
     if (req.body?.image && req.body.image !== asset.image) {
         let uploadImage;
 
-        await cloudinary.uploader.upload(req.body?.image,
-            { public_id: "nftartuseredit" }, 
-            function(error, result) { 
-                console.log(result.secure_url);
-                return uploadImage = result.secure_url
+        // await cloudinary.uploader.upload(req.body?.image,
+        //     { public_id: "nftartuseredit" }, 
+        //     function(error, result) { 
+        //         console.log(result.secure_url);
+        //         return uploadImage = result.secure_url
+        //     });
+        
+        
+            //imgage = > base64
+            return new Promise((resolve, reject) => {
+              cloudinary.uploader.upload(req?.body?.image, opts, (error, result) => {
+                if (result && result.secure_url) {
+                  console.log(result.secure_url);
+                  uploadImage = result.secure_url; 
+                  asset.image = uploadImage     
+                  return resolve(result.secure_url);
+                }
+                console.log(error.message);
+                return reject({ message: error.message });
+              });
             });
-           
+       
 
-        asset.image = uploadImage     
+
+        return 
     }
     if (req.body?.name) asset.name = req.body.name
     if (req.body?.price) asset.price = req.body.price
